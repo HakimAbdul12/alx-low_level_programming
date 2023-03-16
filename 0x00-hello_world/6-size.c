ce9b9c4f873f6eea6e5c00d14954362957865db1
hakim <hakim@gmail.com>
2023-03-16 11:07:03 -0700

    update

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * size of float
+ * size of char
+ * size of long int
+ * size of long long int
+ * size of int
+ */
+int main(void)
+{ printf("Size of char:%d",sizeof(char));
+  printf("Size of float:%d",sizeof(float));
+  printf("Size of long int:%d",sizeof(long int));
+  printf("Size of long long:%d",sizeof(long long int));
+  printf("Size of int:%d",sizeof(int));
+    return (0);
+}
