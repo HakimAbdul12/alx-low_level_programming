52047ee56750ba7d80d4490f7920640149a2d7e1
hakim <hakim@gmail.com>
2023-03-18 15:26:33 -0700

    charle

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -18,8 +18,11 @@ int main(void)
 			{
 			putchar('0' + i);
 			putchar('0' + j);
-			putchar(',');
-			putchar(' ');
+			if (i != 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
 			}
 		}
 	}
