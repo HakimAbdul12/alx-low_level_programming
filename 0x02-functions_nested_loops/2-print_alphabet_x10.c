f2c5c9ce412abb5956050de0b5cfc7bf846dbab6
hakim <hakim@gmail.com>
2023-03-21 09:09:59 -0700

    new one

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Return: 1 on success
+ */
+void print_alphabet_x10(void)
+{
+	int i = 1;
+	while (i <= 10)
+	{
+		char j = 'a';
+		while (j <= 'z')
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
