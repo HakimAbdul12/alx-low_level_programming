649fc34e6981a93fefb1c250298a5c196f57d3e3
hakim <hakim@gmail.com>
2023-03-21 11:34:29 -0700

    updated

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * print_sign - Entry point
+ * Return: Always 0 success
+ * @n: Number to be checked
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+}
