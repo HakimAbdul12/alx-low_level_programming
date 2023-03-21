142ec47d77b1fca7580ba3d310ffdb9815f2e077
hakim <hakim@gmail.com>
2023-03-21 09:48:25 -0700

    updated

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,13 +8,18 @@
 void print_alphabet_x10(void)
 {
 	int i = 1;
+
 	while (i <= 10)
 	{
 		char j = 'a';
+
 		while (j <= 'z')
 		{
 			_putchar(j);
+			j++;
 		}
+
 		_putchar('\n');
+		i++;
 	}
 }
