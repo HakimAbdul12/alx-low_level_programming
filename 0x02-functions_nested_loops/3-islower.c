08f6002bdeb9445d1e36363cdd5876d642048516
hakim <hakim@gmail.com>
2023-03-21 10:21:15 -0700

    own

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * _islower - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int _islower(int c)
+{
+	if (c <= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
