ec458a80e37337c0a42303a9b1fcd4298a0105e9
hakim <hakim@gmail.com>
2023-03-21 15:16:22 -0700

    void print_to_9

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,19 @@
+#include <stdio.>
+/**
+ * print_to_98 - entry
+ * return: always important
+ */
+void print_to_98(int n)
+{
+	for ( int i = n ; i <= 98 ; i++)
+	{
+		printf("%d", i);
+		if (i != 98)
+		{
+			printf(",");
+			printf(" ");
+		}
+	}
+	printf("\n");
+	return (n);
+}
