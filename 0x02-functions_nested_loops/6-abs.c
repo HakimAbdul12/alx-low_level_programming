919a097ca19584aa657d66b7a26ef8e7f6b83307
hakim <hakim@gmail.com>
2023-03-21 13:45:09 -0700

    not now

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * _abs - Entry point
+ * Return: an integer
+ */
+int _abs(int)
+{
+	int k;
+
+	if (k == 0)
+	{
+		_putchar(k +'0');
+	}
+	else if (k > 0)
+	{
+		_putchar(k + '0');
+	}
+	else if (k < 0)
+	{
+		_putchar(k +'0');
+	}
+	return (0);
+}
