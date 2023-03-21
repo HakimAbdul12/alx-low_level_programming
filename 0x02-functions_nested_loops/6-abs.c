6184937e1cb16cb4e1f3f1b2bfc0c465c73def21
hakim <hakim@gmail.com>
2023-03-21 13:52:54 -0700

    not now

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,20 +3,21 @@
 /**
  * _abs - Entry point
  * Return: an integer
+ * @k: the input value
  */
 int _abs(int k)
 {
 	if (k == 0)
 	{
-		_putchar(k +'0');
+		_putchar(k);
 	}
 	else if (k > 0)
 	{
-		_putchar(k + '0');
+		_putchar(k);
 	}
 	else if (k < 0)
 	{
-		_putchar(k +'0');
+		_putchar(k);
 	}
 	return (0);
 }
