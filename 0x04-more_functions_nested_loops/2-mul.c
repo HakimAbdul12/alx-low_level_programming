8ad23acde43da02c1de71a47cf6f6ab393ad8677
hakim <hakim@gmail.com>
2023-03-24 08:50:54 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - is the entry point
+ * @a: is an argument
+ * @b: is also an input argument
+ * return: always 0 success
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
