98aa103a50aa3c99a6f768f1ad8b7cd50c4b0977
hakim <hakim@gmail.com>
2023-03-24 16:43:17 -0700

    new

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,11 @@
+#ifndef main_h
+#define main_h
+
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
 int _putchar(char c);
 void more_numbers(void);
+
+#endif
