050fb149ba80571c2ae365f0f04d0892cff8fb86
hakim <hakim@gmail.com>
2023-03-27 09:59:24 -0700

    print

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts - Prints a string to the screen
+ * @str: is the input
+ * Return: void
+ */
+void _puts(char *str)
+{
+	while (str != '\0')
+	{
+		_putchar(str);
+		str++;
+	}
+	putchar('\n');
+}
