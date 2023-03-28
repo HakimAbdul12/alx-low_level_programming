a8bc36f4ad73e98990e6c37376f1fffb10b947ca
hakim <hakim@gmail.com>
2023-03-28 02:33:32 -0700

    my work

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * *_strcpy - copies onr file to another
+ * @dest: is the buffer pointer
+ * @src: is the original string
+ * Return: Dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	while (*src <= '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	return (dest);
+}
