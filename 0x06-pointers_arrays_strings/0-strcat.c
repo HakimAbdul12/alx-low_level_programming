6be8977d3181d750392e7619240178b42b50fa6c
hakim <hakim@gmail.com>
2023-03-30 17:59:31 +0000

    work hard

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -15,15 +15,11 @@ char *_strcat(char *dest, char *src)
 	int src_len = 0;
 	while (src[src_len] != '\0')
 	{
+		dest[dest_len] = src[src_len];
 		src_len++;
-	}
-	int k;
-
-	for (k = 0; k < src_len; k++)
-	{
-		dest[dest_len + k] = src[k];
+		dest_len++;
 	}
 	dest[dest_len] = '\0';
-	_putchar('\n');
 	return (dest);
 }
+
