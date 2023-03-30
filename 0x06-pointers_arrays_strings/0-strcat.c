7b27b67c4c71329cf1a064ee529c4013878f113c
hakim <hakim@gmail.com>
2023-03-30 17:24:05 +0000

    work hard

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * *_strcat - concatinate one string to another
+ * @dest: the destination
+ * @src: is the source string
+ * Return: character varaible
+ */
+char *_strcat(char *dest, char *src)
+{
+	int dest_len = 0;
+	while (*dest != '\0')
+	{
+		dest++;
+		dest_len++;
+	}
+	int src_len = 0;
+	while (*src != '\0')
+	{
+		src++;
+		src_len++;
+	}
+	int k;
+
+	for (k = 0; k <= src_len; k++)
+	{
+		dest[dest_len + k] = src[k];
+	}
+	dest[dest_len + i] = '\0';
+	return (dest);
+}
