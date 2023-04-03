d90810b3bb8bde02947e3f89159ab4ca2114c18a
hakim <hakim@gmail.com>
2023-04-03 20:57:41 +0000

    string search

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strchr - finds a character in a string
+ * @s: is the pointer to the string
+ * @c: is the character under search
+ * Return: NULL or pointer tto the first occurance of c
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	for (; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (&s[i]);
+		}
+	}
+	return NULL;		
+}
