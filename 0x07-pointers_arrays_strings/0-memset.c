a29a3bbf7fc17cca7516a331b3e278559d4df4b2
hakim <hakim@gmail.com>
2023-04-03 12:57:20 -0700

    correction

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * *_memset - set a block of memory for a byte
+ * _memset - set a block of memory for a byte
  * @s: block where the byte(b) is stored
  * @b: is the byte to be stored
  * @n: is the number of bytes to be stored
