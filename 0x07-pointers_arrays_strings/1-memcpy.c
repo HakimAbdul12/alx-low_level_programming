6f3ebc8cae580b3723e22234e041adfc3603f836
hakim <hakim@gmail.com>
2023-04-03 20:36:25 +0000

    update

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memcpy - copy n bytes
+ * @dest: is the destination to paste the cpoied bytes
+ * @src: is the source
+ * n: is the number of bytes to be copied
+ * Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i = 0;
+
+	for (; n > 0; i++)
+	{
+		dest[i] = src[i];
+		n--;
+	}
+	return (dest);
+}
