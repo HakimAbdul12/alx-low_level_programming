6dd4547663ebe29ca3ce984bbb9b6c8eaa4de030
hakim <hakim@gmail.com>
2023-04-06 00:58:23 +0000

    one of my own

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _pow_recursion - finds x to the power of y
+ * @x: is the base number
+ * @y: is the power
+ * Return: x power y
+ */
+int _pow_recursion(int x, int y)
+{
+	if ( y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y-1));
+	}
+}
