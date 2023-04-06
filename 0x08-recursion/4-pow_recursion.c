ed13a3d23bb94e26fa6a6b324c81482f47af24a7
hakim <hakim@gmail.com>
2023-04-06 01:00:18 +0000

    one of my own

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -7,7 +7,7 @@
  */
 int _pow_recursion(int x, int y)
 {
-	if ( y < 0)
+	if (y < 0)
 	{
 		return (-1);
 	}
@@ -17,6 +17,6 @@ int _pow_recursion(int x, int y)
 	}
 	else
 	{
-		return (x * _pow_recursion(x, y-1));
+		return (x * _pow_recursion(x, y - 1));
 	}
 }
