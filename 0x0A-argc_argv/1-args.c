b71888f653bfb916430a656d0109d3305ea1b097
hakim <hakim@gmail.com>
2023-04-08 02:39:30 +0000

    new one of mine

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - is the entry point
+ * @argc: number og arguments
+ * @argv: is the pointer to the arguments
+ * Return: Always (0) success
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc-1);
+	return (0);
+}
