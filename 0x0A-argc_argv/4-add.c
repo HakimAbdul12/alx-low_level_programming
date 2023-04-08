346a0db0c02432c363a71c4ed750bc63ccbaf668
hakim <hakim@gmail.com>
2023-04-08 22:33:12 +0000

    now is the time

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * add_all - is a function that add all the numbers
+ * @argc: is the argument count
+ * @argv: is the argument vector
+ * Return: always (0) success
+ */
+int add_all(int argc, char *argv[])
+{
+	int i;
+
+	int j;
+
+	int sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		char *y[] = argv[i];
+
+		for (j = 0; y[j] != '\0'; j++)
+		{
+			if (y[j] < '0' || y[j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+			int num = atoi(y[j]);
+			if (num > 0)
+			{
+				sum += num;
+			}
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
+/**
+ * main - is the entry point
+ * @argc: is the argument count
+ * @argv: is the argument vector
+ * Return: always (0) success
+ */
+int main(int argc, char *argv[])
+{
+	if (argc > 1)
+	{
+		return ( add_all(argc, argv));
+	}
+	else
+	{
+		printf("0\n");
+		return (0);
+	}
+}
