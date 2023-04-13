f00765598f4d645a0e029ee7e3f98d251dafe85f
hakim <hakim@gmail.com>
2023-04-13 00:24:21 +0000

    compilation complete

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -7,10 +7,10 @@
  */
 void free_grid(int **grid, int height)
 {
-    int i;
-
-    for (i = 0; i < height; i++) {
-        free(grid[i]);
-    }
-    free(grid);
+	int i;
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
 }
