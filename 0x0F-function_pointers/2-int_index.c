a1a9425f612cfaf7aa15dc7e5a10e304688f9026
hakim <hakim@gamil.com>
2023-04-19 13:42:52 +0000

    new update on function pointers

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,7 +12,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int k;
 	int y;
 
-	if (array == NULL || size <= 0 || cmp == NULL)
+	if (array == NULL || cmp == NULL)
 		exit(1);
 
 	if (size <= 0)
