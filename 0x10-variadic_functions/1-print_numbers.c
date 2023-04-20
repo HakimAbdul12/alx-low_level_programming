667003aea3e60beebc1525644e2adc3abd1efad4
hakim <hakim@gamil.com>
2023-04-20 21:04:25 +0000

    vara functions

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+ * print_numbers - prints numbers
+ * @separator is the pointer to the string to be printed btwen the numbers
+ * @n: is the number of args to be printed
+ * Return: Nothing
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list list;
+
+	va_start(list, n);
+	for (i = 0; i < n; i++)
+	{
+		if (separator == NULL)
+		{
+			if (i == (n-1))
+				printf("%d\n", va_arg(list, int));
+			else
+				printf("%d", va_arg(list, int));
+		}
+		else
+		{
+			if (i == (n - 1))
+				printf("%d\n", va_arg(list, int));
+			else
+				printf("%d%s", va_arg(list, int), separator);
+		}
+	}
+	va_end(list);
+}
