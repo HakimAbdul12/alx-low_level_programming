ee983d0977ef2d42c2abee7e10b4c1685b96b13b
hakim <hakim@gamil.com>
2023-04-21 00:41:15 +0000

    doja

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -18,7 +18,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	{
 		str = va_arg(list, char *);
 		if (str == NULL)
-			printf("nil");
+			printf("(nil)");
 		else
 			printf("%s", str);
 
