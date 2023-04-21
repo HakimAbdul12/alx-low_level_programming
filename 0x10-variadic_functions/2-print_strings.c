2e1ebe50ae5f4f4891278d035023d65a4467d2a3
hakim <hakim@gamil.com>
2023-04-21 00:39:47 +0000

    doja

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -25,6 +25,6 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		if (separator != NULL && i != (n - 1))
 			printf("%s", separator);
 	}
-	va_end(list);
 	printf("\n");
+	va_end(list);
 }
