e301244eb91f15656920e4cf456fe9191fa64244
Hakim <abdulhakim6490@gamil.com>
2023-04-27 23:58:59 +0000

    my wn

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -21,7 +21,7 @@ list_t *add_node(list_t **head, const char *str)
 	if (new_node->str == NULL)
 	{
 		free(new_node);
-		return(NULL);
+		return (NULL);
 	}
 	new_node->len = strlen(str);
 	return (*head);
