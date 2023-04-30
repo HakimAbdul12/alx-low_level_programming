6efc59cd5b6e4c82f12c7db8ac063481dd52959d
Hakim <abdulhakim6490@gamil.com>
2023-04-30 21:30:37 +0000

    correction

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -14,7 +14,7 @@ size_t print_listint(const listint_t *h)
 		return (0);
 	while (h != NULL)
 	{
-		printf("%d\n",h->n);
+		printf("%d\n", h->n);
 		count++;
 		h = h->next;
 	}
--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -3,6 +3,7 @@
 #include <string.h>
 /**
  * listint_len - return the number of elements in a list
+ * @h: is the pointer to the head node
  * Return: number od elements or nodes
  */
 size_t listint_len(const listint_t *h)
