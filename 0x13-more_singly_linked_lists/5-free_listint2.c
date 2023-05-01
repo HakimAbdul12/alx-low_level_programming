320c99b4f22738bb3bb62ca20da882d90f3e8b58
Hakim <abdulhakim6490@gamil.com>
2023-05-01 02:18:06 +0000

    more changes

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,17 +7,16 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *current = *head;
 	listint_t *var;
 
 	if (head == NULL)
 		return;
 
-	while (current != NULL)
+	while (*head)
 	{
-		var = current->next;
-		free(current);
-		current = var;
+		var = (*head)->next;
+		free(*head);
+		*head = var;
 	}
 	*head = NULL;
 }
