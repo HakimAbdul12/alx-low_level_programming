c7c2a6fbc27079b7d817b1b6284b80c1dba84077
Hakim <abdulhakim6490@gamil.com>
2023-05-01 01:08:51 +0000

    free list

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 /**
  * free_listint - frees all the members of the linked list
+ * @head: is the pointer to the head node
  * Return: nothing
  */
 void free_listint(listint_t *head)
